469d1ab34d6303f513437319d5e10c8df3529565
CodeBasePW <coderatorman@gmail.com>
2020-03-09 15:33:22 -0700

    Added wrapper class for the minecraft.net session API

---
+++ src/me/moderator_man/poseidon/SessionAPI.java
@@ -0,0 +1,54 @@
+package me.moderator_man.poseidon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * A wrapper class for the Minecraft session API
+ * TODO maybe make the HTTP requests asynchronous? idk if it really matters
+ * @author moderator_man
+ */
+public class SessionAPI
+{
+	public static final String SESSION_BASE = "http://session.minecraft.net/game/";
+	
+	public static boolean hasJoined(String username, String serverId)
+	{
+		String response = httpGetRequest(SESSION_BASE + String.format("checkserver.jsp?user=%s&serverId=%s", username, serverId));
+		if (response != "YES")
+			return false;
+		return true;
+	}
+	
+	public static boolean hasJoined(String username, String serverId, String ip)
+	{
+		String response = httpGetRequest(SESSION_BASE + String.format("checkserver.jsp?user=%s&serverId=%s&ip=", username, serverId, ip));
+		if (response != "YES")
+			return false;
+		return true;
+	}
+	
+	private static String httpGetRequest(String url)
+	{
+		try
+		{
+			URL obj = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", "Project-Poseidon/0");
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null)
+				response.append(inputLine);
+			in.close();
+			return response.toString();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return "";
+		}
+	}
+}
