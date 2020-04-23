727d874799cf0382cf77924dabba13befac1c6ff
Johny Muffin <jetpackingwolf@gmail.com>
2020-04-23 16:59:10 +1000

    Update already only disconnect message

--- src/com/projectposeidon/johnymuffin/LoginProcessHandler.java
+++ src/com/projectposeidon/johnymuffin/LoginProcessHandler.java
@@ -101,7 +101,7 @@ public class LoginProcessHandler {
         //Check if a player with the same UUID or Username is already online which is mainly an issue in Offline Mode servers.
         for (Player p : server.getOnlinePlayers()) {
             if (p.getName().equalsIgnoreCase(username) || p.getUniqueId().equals(uuid)) {
-                cancelLoginProcess(ChatColor.RED + "A player with your username is already online");
+                cancelLoginProcess(ChatColor.RED + "A player with your username or uuid is already online, try reconnecting in a minute.");
                 System.out.println("User " + username + " has been blocked from connecting as they share a username or UUID with a user who is already online called " + p.getName() +
                         "\nMost likely the user has changed their UUID or the server is running in offline mode and someone has attempted to connect with their name");
             }
