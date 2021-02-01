0107be8238912445a1c3b69e4368e3d6f0a94fc4
RhysB <github@johnymuffin.com>
2021-02-01 11:40:07 +1100

    Add TCP No Delay option and prepare for 1.1.6 release

--- src/net/minecraft/server/NetworkManager.java
+++ src/net/minecraft/server/NetworkManager.java
@@ -1,5 +1,7 @@
 package net.minecraft.server;
 
+import com.legacyminecraft.poseidon.PoseidonConfig;
+
 import java.io.BufferedOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -51,6 +53,9 @@ public class NetworkManager {
         try {
             // CraftBukkit start - cant compile these outside the try
             socket.setSoTimeout(30000);
+            if (PoseidonConfig.getEmptyNode().getBoolean("settings.enable-tpc-nodelay", false)) {
+                socket.setTcpNoDelay(true);
+            }
             this.input = new DataInputStream(socket.getInputStream());
             this.output = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream(), 5120));
         } catch (java.io.IOException socketexception) {
