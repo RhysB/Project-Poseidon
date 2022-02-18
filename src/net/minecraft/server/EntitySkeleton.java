55b5fb002f32ac1f7b587a51bf9c698ef7b82246
Strultz <strultzgaming@gmail.com>
2022-02-17 20:10:18 -0600

    Fix skeleton bow sound

--- src/net/minecraft/server/EntitySkeleton.java
+++ src/net/minecraft/server/EntitySkeleton.java
@@ -57,7 +57,7 @@ public class EntitySkeleton extends EntityMonster {
                 double d2 = entity.locY + (double) entity.t() - 0.20000000298023224D - entityarrow.locY;
                 float f1 = MathHelper.a(d0 * d0 + d1 * d1) * 0.2F;
 
-                this.world.makeSound(this, "random.bow", 1.0F, 1.0F / (this.random.nextFloat() * 0.4F + 0.8F));
+                this.world.e(1002, MathHelper.floor(this.locX), MathHelper.floor(this.locY - (double)this.height), MathHelper.floor(this.locZ), 0); // Poseidon - fix skeleton bow sounds (Strultz)
                 entityarrow.a(d0, d2 + (double) f1, d1, 0.6F, 12.0F);
                 this.world.addEntity(entityarrow);
                 this.attackTicks = 30;
