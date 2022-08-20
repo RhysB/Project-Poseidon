6b9d351fc7949d2bcabc214eaaa838d6e616c673
Strultz <strultzgaming@gmail.com>
2022-08-20 13:12:17 -0500

    Bungee mode??????

--- src/net/minecraft/server/NetLoginHandler.java
+++ src/net/minecraft/server/NetLoginHandler.java
@@ -108,6 +108,12 @@ public class NetLoginHandler extends NetHandler {
             }
             rawConnectionType = packet1login.d;
             //TODO: We need to find a better and cleaner way to support these different Beta proxies, Maybe a handler class???
+			if ((Boolean) PoseidonConfig.getInstance().getConfigOption("settings.bungeecord.bungee-mode.enable") && !connectionType.equals(ConnectionType.BUNGEECORD_OFFLINE_MODE_IP_FORWARDING) && !connectionType.equals(ConnectionType.BUNGEECORD_ONLINE_MODE_IP_FORWARDING)) {
+				a.info(packet1login.name + " is not using BungeeCord, kicking the player.");
+				this.disconnect((String) PoseidonConfig.getInstance().getConfigOption("settings.bungeecord.bungee-mode.kick-message"));
+				return;
+			}
+			
             if (connectionType.equals(ConnectionType.RELEASE2BETA_OFFLINE_MODE_IP_FORWARDING) || connectionType.equals(ConnectionType.RELEASE2BETA_ONLINE_MODE_IP_FORWARDING) || connectionType.equals(ConnectionType.BUNGEECORD_OFFLINE_MODE_IP_FORWARDING) || connectionType.equals(ConnectionType.BUNGEECORD_ONLINE_MODE_IP_FORWARDING)) {
                 //Proxy has IP Forwarding enabled
                 if ((Boolean) PoseidonConfig.getInstance().getConfigOption("settings.release2beta.enable-ip-pass-through")) {
