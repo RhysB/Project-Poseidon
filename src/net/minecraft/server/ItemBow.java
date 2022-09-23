0774a18cf311fb60b1f30478443fcc8b5dfe0048
Strultz <69481716+Strultz@users.noreply.github.com>
2022-09-23 05:36:21 -0500

    Bow sound for players fix
    from uberbukkit??

--- src/net/minecraft/server/ItemBow.java
+++ src/net/minecraft/server/ItemBow.java
@@ -1,5 +1,7 @@
 package net.minecraft.server;
 
+import com.legacyminecraft.poseidon.PoseidonConfig;
+
 public class ItemBow extends Item {
 
     public ItemBow(int i) {
@@ -9,7 +11,11 @@ public class ItemBow extends Item {
 
     public ItemStack a(ItemStack itemstack, World world, EntityHuman entityhuman) {
         if (entityhuman.inventory.b(Item.ARROW.id)) {
-            world.makeSound(entityhuman, "random.bow", 1.0F, 1.0F / (b.nextFloat() * 0.4F + 0.8F));
+            if ((boolean) PoseidonConfig.getInstance().getProperty("world.settings.skeleton-shooting-sound-fix.enabled")) {
+                world.e(1002, MathHelper.floor(entityhuman.locX), MathHelper.floor(entityhuman.locY - (double)entityhuman.height), MathHelper.floor(entityhuman.locZ), 0); // Poseidon - fix player bow sounds (Strultz)
+            } else {
+                world.makeSound(entityhuman, "random.bow", 1.0F, 1.0F / (b.nextFloat() * 0.4F + 0.8F));
+            }
             if (!world.isStatic) {
                 world.addEntity(new EntityArrow(world, entityhuman));
             }
