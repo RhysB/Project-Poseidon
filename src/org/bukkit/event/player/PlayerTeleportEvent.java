9dfc715fb346bc854ff36297b09c925a7148ec64
Johny Muffin <jetpackingwolf@gmail.com>
2023-03-18 11:52:41 +1000

    Cross dimension dupe fix

--- src/org/bukkit/event/player/PlayerTeleportEvent.java
+++ src/org/bukkit/event/player/PlayerTeleportEvent.java
@@ -1,6 +1,8 @@
 package org.bukkit.event.player;
 
+import net.minecraft.server.EntityPlayer;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 
@@ -10,9 +12,23 @@ import org.bukkit.event.Event;
 public class PlayerTeleportEvent extends PlayerMoveEvent {
     public PlayerTeleportEvent(Player player, Location from, Location to) {
         super(Type.PLAYER_TELEPORT, player, from, to);
+        blockCrossDimensionDupe(); //Poseidon
     }
 
     public PlayerTeleportEvent(final Event.Type type, Player player, Location from, Location to) {
         super(type, player, from, to);
+        blockCrossDimensionDupe(); //Poseidon
     }
+
+    //Poseidon - Start
+    private void blockCrossDimensionDupe() {
+        if (this.getFrom().getWorld() != this.getTo().getWorld()) {
+            EntityPlayer entity = ((CraftPlayer) this.getPlayer()).getHandle();
+            if (entity.activeContainer == entity.defaultContainer)
+                return;
+            System.out.println("[Poseidon] Force closing " + player.getName() + "'s inventory as they have teleported to a different world. This is to prevent a dupe bug.");
+            entity.y();
+        }
+    }
+    //Poseidon - End
 }
