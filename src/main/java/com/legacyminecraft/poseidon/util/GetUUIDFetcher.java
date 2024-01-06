e23b136b7f26373cf5dfb4c6377b6c9b766d6ec5
Qugalet <qugalet@m0e.space>
2024-01-07 01:45:49 +0200

    Fix cracked account login (#79)
    * fix cracked account login
    
    * remove debug info

--- src/main/java/com/legacyminecraft/poseidon/util/GetUUIDFetcher.java
+++ src/main/java/com/legacyminecraft/poseidon/util/GetUUIDFetcher.java
@@ -56,7 +56,7 @@ public class GetUUIDFetcher {
 
         } catch (Exception exception) {
             UUIDResult uuidResult = null;
-            if (exception == null || exception.getMessage() == null) {
+            if (exception == null || exception instanceof FileNotFoundException || exception.getMessage() == null) {
                 // This is a hacky solution to tell if the API is offline, or the user is cracked.
                 uuidResult = new UUIDResult(generateOfflineUUID(username), UUIDResult.ReturnType.OFFLINE);
             } else {
