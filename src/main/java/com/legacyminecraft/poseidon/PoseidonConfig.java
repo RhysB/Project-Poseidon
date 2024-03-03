e5353848874837e9879bca07fd48fa5b521ee71f
Johny Muffin <jetpackingwolf@gmail.com>
2024-03-03 22:20:31 +1000

    Configurable mob spawner area limit

--- src/main/java/com/legacyminecraft/poseidon/PoseidonConfig.java
+++ src/main/java/com/legacyminecraft/poseidon/PoseidonConfig.java
@@ -104,6 +104,14 @@ public class PoseidonConfig extends Configuration {
         generateConfigOption("world.settings.speed-hack-check.teleport", true);
         generateConfigOption("world.settings.speed-hack-check.distance", 100.0D);
         generateConfigOption("world.settings.speed-hack-check.info", "This setting allows you to configure the automatic speedhack detection.");
+        //Mob Spawner Area Limit (8 chunks)
+        generateConfigOption("world.settings.mob-spawner-area-limit.enable", true);
+        generateConfigOption("world.settings.mob-spawner-area-limit.limit", 150);
+        generateConfigOption("world.settings.mob-spawner-area-limit.chunk-radius", 8);
+        generateConfigOption("world.settings.mob-spawner-area-limit.info",
+                "This setting controls the maximum number of entities of a mob spawner type that can exist within the defined chunk radius around a mob spawner. If the number of entities exceeds this limit, the spawner will stop spawning additional entities of that type. This is useful to stop the extreme lag that can be caused by mob spawners.");
+
+
         //generateConfigOption("world-settings.eject-from-vehicle-on-teleport.enabled", true);
         //generateConfigOption("world-settings.eject-from-vehicle-on-teleport.info", "Eject the player from a boat or minecart before teleporting them preventing cross world coordinate exploits.");
 
